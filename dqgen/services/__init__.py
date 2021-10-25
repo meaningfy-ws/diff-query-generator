e85f729fd76922ea6b8139028900c9827ebac757
Dragos0000 <dragos.paun0000@gmail.com>
2021-10-25 16:31:17 +0100

    added queries in mappings and merged in main branch

--- dqgen/services/__init__.py
+++ dqgen/services/__init__.py
@@ -13,6 +13,15 @@ from dqgen.services.query_template_registry import QueryTemplateRegistry
 CLASSES_OPERATION_TEMPLATE_MAPPING = {"added_instance": QueryTemplateRegistry().INSTANCE_ADDITIONS,
                                       "deleted_instance": QueryTemplateRegistry().INSTANCE_DELETIONS}
 PROPERTIES_OPERATION_TEMPLATE_MAPPING = {"added_property": QueryTemplateRegistry().PROPERTY_ADDITIONS,
-                                         "deleted_property": QueryTemplateRegistry().PROPERTY_DELETIONS}
+                                         "deleted_property": QueryTemplateRegistry().PROPERTY_DELETIONS,
+                                         "value_update_property": QueryTemplateRegistry().PROPERTY_VALUE_UPDATES,
+                                         "movement_cross_instance": QueryTemplateRegistry().PROPERTY_MOVEMENT_CROSS_INSTANCE,
+                                         "movement_cross_property": QueryTemplateRegistry().PROPERTY_MOVEMENT_CROSS_PROP
+
+                                         }
 REIFIED_PROPERTIES_OPERATION_TEMPLATE_MAPPING = {"added_reified": QueryTemplateRegistry().REIFIED_PROPERTY_ADDITIONS,
-                                                 "deleted_reified": QueryTemplateRegistry().REIFIED_PROPERTY_DELETIONS}
+                                                 "deleted_reified": QueryTemplateRegistry().REIFIED_PROPERTY_DELETIONS,
+                                                 "value_update_reified": QueryTemplateRegistry().REIFIED_PROPERTY_VALUE_UPDATES,
+                                                 "reified_movement_cross_instance": QueryTemplateRegistry().PROPERTY_MOVEMENT_CROSS_INSTANCE,
+                                                 "reified_movement_cross_property": QueryTemplateRegistry().PROPERTY_MOVEMENT_CROSS_PROP
+                                                 }
